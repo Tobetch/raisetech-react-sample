faaff7a279df7b66927430b6c440445d6bf10eec
Tobetch <takutobe@gmail.com>
2022-05-07 16:46:10 +0900

    add userList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import axios from "axios";
+
+const baseURL = "http://localhost:8080/api";
 
 function App() {
+
+  const [users, setUsers] = React.useState(null);
+
+  React.useEffect(() => {
+
+    // API実行した体
+    // const dummyUserList = [
+    //   {
+    //     "id": 1,
+    //     "name": "testtest",
+    //     "age": "11"
+    //   },
+    //   {
+    //     "id": 2,
+    //     "name": "test",
+    //     "age": "2"
+    //   },
+    //   {
+    //     "id": 3,
+    //     "name": "testtest",
+    //     "age": "3"
+    //   },
+    //   {
+    //     "id": 4,
+    //     "name": "test氏名",
+    //     "age": "4"
+    //   },
+    //   {
+    //     "id": 5,
+    //     "name": "tobetch",
+    //     "age": "5"
+    //   },
+    //   {
+    //     "id": 6,
+    //     "name": "tobetch",
+    //     "age": "6"
+    //   },
+    //   {
+    //     "id": 7,
+    //     "name": "tobetch",
+    //     "age": "7"
+    //   }
+    // ];
+
+    // setUsers(dummyUserList);
+    // console.info(dummyUserList);
+
+    axios.get(baseURL + "/user").then((response) => {
+      console.log(response);
+      setUsers(response.data);
+    }).catch(error => {
+      console.error(error);
+    });
+  }, []);
+
+  // if (!users) return "No post!"
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +78,16 @@ function App() {
         >
           Learn React
         </a>
+        {users?.map(
+          (user) =>
+            <div>
+              <p>{user.id}</p>
+              <p>{user.name}</p>
+              <p>{user.age}</p>
+            </div>
+        )
+        }
+        {/* {error && <div>エラー発生</div>} */}
       </header>
     </div>
   );
